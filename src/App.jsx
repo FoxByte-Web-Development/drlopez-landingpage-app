87ebe6f86bbc7562045bac7c865430e32b2798ba
Fernando Reyes <73537128+fernandort10@users.noreply.github.com>
2022-11-16 15:42:31 -0400

    prettier folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { useState } from "react";
-import Button from "./Components/Button/button-componet";
-import "./App.css";
+import { useState } from 'react'
+import Button from './Components/Button/button-componet'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world with tailwind!{" "}
-      </h1>
-      <Button>Button fill</Button>
-      <Button variant="secondary">Button with border</Button>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1 className="text-3xl font-bold underline">
+                Hello world with tailwind!{' '}
+            </h1>
+            <Button>Button fill</Button>
+            <Button variant="secondary">Button with border</Button>
+        </div>
+    )
 }
 
-export default App;
+export default App
