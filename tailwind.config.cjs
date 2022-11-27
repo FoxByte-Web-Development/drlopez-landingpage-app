8c3ec8ed00603eccae587dc5f1ed3f596a718c10
Ricardo Arturo <49619270+Ricaidito@users.noreply.github.com>
2022-11-27 10:05:14 -0400

    Added colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,12 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      "teal-blue": "#7ac5c8",
+      "teal-1": "#378972",
+      "teal-2": "#7ac5c8",
+      "purple-1": "#643b5b",
+      "purple-2": "#bc5985",
+      "purple-3": "#c67a98",
+      "purple-4": "#eeaec6",
     },
     extend: {},
   },
