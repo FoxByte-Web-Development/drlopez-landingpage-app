c2b767b9b1f88ce05c301da9aaa17f975b00e5f9
Ricardo Arturo <49619270+Ricaidito@users.noreply.github.com>
2022-12-15 17:18:28 -0400

    Lang feature

--- src/Components/language-selector/language-selector.jsx
+++ src/Components/language-selector/language-selector.jsx
@@ -1,21 +1,21 @@
 import { useContext } from "react";
 import { languageOptions } from "../../languages/language-frame";
 import { LanguageContext } from "../../contexts/language-context/language-context";
+import Dropdown from "../Dropdown/dropdown";
 
 export default function LanguageSelector() {
   const { userLanguage, userLanguageChange } = useContext(LanguageContext);
+  const languages = Object.entries(languageOptions);
 
-  const handleLanguageChange = e => {
-    userLanguageChange(e.target.value);
+  const handleLanguageChange = lang => {
+    userLanguageChange(lang);
   };
 
   return (
-    <select onChange={handleLanguageChange} value={userLanguage}>
-      {Object.entries(languageOptions).map(([id, name]) => (
-        <option key={id} value={id}>
-          {name}
-        </option>
-      ))}
-    </select>
+    <Dropdown
+      selected={userLanguage}
+      setSelected={handleLanguageChange}
+      optionsToSelect={languages}
+    />
   );
 }
